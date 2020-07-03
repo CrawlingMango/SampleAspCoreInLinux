f2b4964a377bf21869a7af007a1d2dec0a77b3c5
CrawlingMango <jeromeavelino307@gmail.com>
2020-07-04 07:25:17 +0800

    added asp console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleAspCoreInLinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
